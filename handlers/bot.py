57e2c308bc2ba93cab4e8e43e024b7120a2225b3
chenxiaobin2486 <space711206@gmail.com>
2025-06-30 12:18:51 +0700

    Update bot.py

--- handlers/bot.py
+++ handlers/bot.py
@@ -2,6 +2,10 @@ import logging
 from telegram.ext import ApplicationBuilder, MessageHandler, filters
 from config import TELEGRAM_TOKEN
 from handlers.command_parser import handle_message
+import os
+
+PORT = int(os.environ.get("PORT", 8443))
+WEBHOOK_URL = f"https://winpay999.onrender.com/{TELEGRAM_TOKEN}"
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -11,8 +15,15 @@ logging.basicConfig(
 def run_bot():
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-    # 所有文本消息都会传给 handle_message 函数
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-    logging.info("🤖 机器人已启动")
-    app.run_polling()
+    logging.info("🤖 机器人已启动（Webhook 模式）")
+
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        webhook_url=WEBHOOK_URL
+    )
+
+if __name__ == "__main__":
+    run_bot()
